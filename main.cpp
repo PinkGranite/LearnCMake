eda0f041298869639cf2e121be72c54fba2aef3a
PinkGranite <617519256@qq.com>
2022-04-06 09:42:59 +0800

    add VERSION control

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "MathLib.h"
+#include <LearnCMakeConf.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char * argv[]) {
+    std::cout << argv[0] << " Version: " << LearnCMake_VERSION_MAJOR << "." << LearnCMake_VERSION_MINOR << '\n';
+
     std::cout << "hello" << std::endl;
     std::cout << add(1, 2) << std::endl;
     return 0;
